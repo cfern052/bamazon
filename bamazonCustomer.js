653b30ba241573718c6baa8f36d4814c52ef90c3
cfern052 <cfern052@gmail.com>
2019-03-07 22:12:09 -0500

    Updated table, added inquiry

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,9 +1,64 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer")
 
 var connection = mysql.createConnection({
-    host:"localhost",
-    port:3306,
-    user:"root",
-    password:"root",
-    database:"bamazon"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "bamazon"
 });
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Welcome to Bamazon. The worlds leading fake amazon site!")
+    listProducts();
+});
+
+function listProducts() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        console.log("~~~~~~~~~~~~~~~~~~~");
+        console.log("Accessing Database");
+        console.log("~~~~~~~~~~~~~~~~~~~");
+        console.log(res);
+        search();
+    });
+
+    function search() {
+        inquirer
+            .prompt({
+                type: "input",
+                message: "Enter the ID of the product you would like to purchase:",
+                name: 'id'
+            },
+                {
+                    type: "input",
+                    message: "Enter the amount you would like to purchase:",
+                    name: "quantity"
+
+                }
+            )
+            .then(function (answer) {
+                connection.query('SELECT * FROM products WHERE product_name = ' + answer.id, function(err, result){
+                    console.log(err)
+     
+                    if(result[0].stock_quantity > parseInt(answer.quantity)){
+     
+                        var newQuantity = parseInt(result[0].stock_quantity) - parseInt(answer.quantity);
+                        con.query("UPDATE products SET stock_quantity = " + newQuantity +" WHERE product_name = " + answer.id, function(err, res){
+                            console.log("Order Successful");
+                            listProducts()
+                        })
+                    }else {
+                        console.log("insufficient quantity")
+                        search()
+                    }
+                })
+
+
+            })
+    }
+}
+
--- seeds.sql
+++ seeds.sql
@@ -2,3 +2,14 @@ insert into bamazon.products(product_name,department_name,price,stock_quantity) 
 ("Paper_Towels","Cleaning",2.50,100),
 ("Blood_Sex_Sugar_Magic","vinyl",20,15),
 ("HDMI_Cables","electronics",10,100)
+
+insert into bamazon.products(product_name,department_name,price,stock_quantity) values
+("Pinesol","Cleaning",5,70),
+("In Rainbows","vinyl",23,12),
+("Xbox One X","electronics",358,250)
+
+insert into bamazon.products(product_name,department_name,price,stock_quantity) values
+("It","Books",12,100),
+("The Stand","Books",7,250),
+("Samsung Soundbar","electronics",120,75),
+("House Of Leaves","Books",15,25)
